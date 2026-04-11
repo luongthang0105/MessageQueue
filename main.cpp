3168d2478748c4c662c0139ab195e75bdfb1c603
Luong Thang Nguyen <z5402969@ad.unsw.edu.au>
2026-04-11 15:50:43 +1000

    feat: draft code for TCP client, save just for future reference

--- main.cpp
+++ main.cpp
@@ -1,11 +1,84 @@
 #include <iostream>
+#include <string>
+#include <vector>
+#include <chrono>
+#include <sys/socket.h>
+#include "spdlog/spdlog.h"
 
-int main() {
-    const auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
+#include <asio.hpp>
+#include <asio/ts/buffer.hpp>
+#include <asio/ts/internet.hpp>
 
-    for (int i = 1; i <= 5; i++) {
-        std::cout << "i = " << i << std::endl;
+std::vector<char> responseBuf(1024);
+void ReadSome(asio::ip::tcp::socket &socket)
+{
+    socket.async_read_some(asio::buffer(responseBuf.data(), responseBuf.size()),
+                           [&](const asio::error_code &ec, std::size_t bytesRead)
+                           {
+                               if (!ec)
+                               {
+                                   SPDLOG_INFO("Bytes read: {}", bytesRead);
+                                   for (int i = 0; i < bytesRead; ++i)
+                                   {
+                                       std::cout << responseBuf[i];
+                                   }
+
+                                   ReadSome(socket);
+                               }
+                               else
+                               {
+                                   SPDLOG_INFO("Error message: {}", ec.message());
+                               }
+                           });
+}
+int main()
+{
+    asio::error_code ec;
+
+    asio::io_context context;
+
+    // fake work to keep context running
+    auto workGuard = asio::make_work_guard(context);
+
+    // let the context run on another thread
+    std::thread thrdCtx{[&]()
+                        { context.run(); }};
+
+    asio::ip::tcp::endpoint endpoint{asio::ip::make_address("52.71.108.149", ec), 80};
+
+    asio::ip::tcp::socket socket(context);
+
+    socket.connect(endpoint, ec);
+
+    if (!ec)
+    {
+        SPDLOG_INFO("Connected!\n");
+    }
+    else
+    {
+        std::cout << "Failed to connect to server: " << ec.message() << "\n";
+        return 0;
+    }
+
+    if (socket.is_open())
+    {
+        ReadSome(socket);
+
+        std::string request =
+            "GET / HTTP/1.1\r\n"
+            "Host: httpbin.org\r\n"
+            "Connection: close\r\n\r\n";
+
+        size_t bytes_written = socket.write_some(asio::buffer(request.data(), request.size()), ec);
+
+        using namespace std::chrono_literals;
+        std::this_thread::sleep_for(10000ms);
+
+        context.stop();
+        if (thrdCtx.joinable())
+        {
+            thrdCtx.join();
+        }
     }
 
     return 0;
