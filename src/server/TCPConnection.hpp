9e790554f184846d045e3f3cdda5a422c5f3071f
Luong Thang Nguyen <z5402969@ad.unsw.edu.au>
2026-07-07 22:52:27 +1000

    refactor: extract TCPConnection implementation to cpp file

--- src/server/TCPConnection.hpp
+++ src/server/TCPConnection.hpp
@@ -2,16 +2,14 @@
 #define SRC_SERVER_TCPCONNECTION_HPP_
 
 #include <memory>
+#include <string>
+
 #include <asio.hpp>
 #include "spdlog/spdlog.h"
-#include "string"
 
 using asio::ip::tcp;
 
-std::string make_daytime_string() {
-    std::time_t now = std::time(0);
-    return std::ctime(&now);
-}
+std::string make_daytime_string();
 
 class TCPConnection : std::enable_shared_from_this<TCPConnection> {
   public:
@@ -21,20 +19,14 @@ class TCPConnection : std::enable_shared_from_this<TCPConnection> {
         return std::shared_ptr<TCPConnection>(new TCPConnection {io_context});
     }
 
-    tcp::socket &get_socket() { return socket_; }
-    void start() {
-        message_ = make_daytime_string();
-        SPDLOG_INFO("Prepare sending message back to client");
-        asio::async_write(socket_, asio::buffer(message_),
-                          std::bind(&TCPConnection::handle_write, this,
-                                    std::placeholders::_1,
-                                    std::placeholders::_2));
-    }
+    tcp::socket& get_socket();
 
-    void handle_write(const asio::error_code &ec, size_t bytes_transferred) {
-        SPDLOG_DEBUG("Try print ec: {}", ec.message());
-        SPDLOG_INFO("I've sent {} bytes back!", bytes_transferred);
-    }
+    /**
+     * @brief On connection, prepare messages and respond back to the client.
+     */
+    void start();
+
+    void handle_write(const asio::error_code &ec, size_t bytes_transferred);
 
   private:
     tcp::socket socket_;
