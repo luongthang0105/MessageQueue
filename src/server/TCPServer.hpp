30047b6ff9a642dd83f6b568d3a56f9f3c919fb1
Luong Thang Nguyen <z5402969@ad.unsw.edu.au>
2026-07-07 21:56:40 +1000

    refactor: extract TCPServer implementation into cpp file

--- src/server/TCPServer.hpp
+++ src/server/TCPServer.hpp
@@ -13,40 +13,20 @@ using asio::ip::tcp;
 constexpr uint16_t PORT_NUM = 10001;
 
 class TCPServer {
+  public:
+    TCPServer();
+    asio::io_context &get_io_context() { return io_context_; }
+
   private:
     asio::io_context io_context_{};
     tcp::acceptor acceptor_{io_context_, tcp::endpoint{tcp::v4(), PORT_NUM}};
 
-    void start_accept() {
-        TCPConnection::ptr conn_ptr =
-            TCPConnection::create_conn_ptr(io_context_);
+    /**
+     * @brief Creates a TCP socket, listening to new connections. Calls @ref TCPServer::handle_accept() after accepting a new connection.
+     */
+    void start_accept();
 
-        acceptor_.async_accept(
-            conn_ptr->get_socket(),
-            std::bind(
-                &TCPServer::handle_accept, this, conn_ptr,
-                std::placeholders::_1));
-    }
-
-    void handle_accept(TCPConnection::ptr new_conn,
-                       const asio::error_code &ec) {
-        SPDLOG_INFO("Accepted a connection!");
-        SPDLOG_DEBUG("Try print ec: {}", ec.message());
-        if (!ec) {
-            new_conn->start();
-        }
-
-        // calling this again to start accepting other connections as well
-        start_accept();
-    }
-
-  public:
-    TCPServer() {
-        SPDLOG_INFO("Starting TCP Server!");
-        start_accept();
-        io_context_.run();
-    }
-    asio::io_context &get_io_context() { return io_context_; }
+    void handle_accept(TCPConnection::ptr new_conn, const asio::error_code &ec);
 };
 
 #endif // SRC_SERVER_TCPSERVER_HPP_
