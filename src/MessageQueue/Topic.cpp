60bb911e779a174ac3798f9524b6de6eac581fbb
Luong Thang Nguyen <z5402969@ad.unsw.edu.au>
2026-07-19 15:58:03 +1000

    fix: use std::format for error msg

--- src/MessageQueue/Topic.cpp
+++ src/MessageQueue/Topic.cpp
@@ -1,25 +1,26 @@
-#include <exception>
-#include <string>
 #include "MessageQueue/Topic.hpp"
+#include <exception>
+#include <format>
+#include <string>
 
-template<typename T>
+template <typename T>
 void Topic<T>::push_item(std::string partition_key, T item) {
     partitions_[partition_key].push_back(item);
 }
 
-template<typename T>
+template <typename T>
 T Topic<T>::get_item(std::string partition_key, size_t offset) const {
     auto partition_it = partitions_.find(partition_key);
     if (partition_it == partitions_.end()) {
-        std::string error_msg = "Partition of key \"" + partition_key + "\" does not exist."
-        throw std::runtime_error(error_msg);
+        throw std::invalid_argument(std::format(
+            "Partition of key \"" {} "\" does not exist.", partition_key));
     }
 
-    Partition& partition = *partition_it;
+    Partition &partition = *partition_it;
     if (offset >= partition.size()) {
-        std::string error_msg = "Partition of key \"" + partition_key + "\" have size=" + partition.size()
-                                + ", where offset=" + offset;
-        throw std::runtime_error(error_msg);
+        throw std::invalid_argument(std::format(
+            "Partition of key \"" {} "\" have size = {}, where offset = ",
+            partition_key, partition.size(), offset));
     }
 
     return partition[partition.size() - offset];
