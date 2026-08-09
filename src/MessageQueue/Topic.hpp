94d0ce84948ccda34f867b38929775bf116561c1
Luong Thang Nguyen <z5402969@ad.unsw.edu.au>
2026-08-09 15:05:21 +1000

    fix: move Topic impl into .hpp file, because template impls have to be in header file

--- src/MessageQueue/Topic.hpp
+++ src/MessageQueue/Topic.hpp
@@ -18,13 +18,33 @@ class Topic {
     /**
      * @brief Push an item to the end of a partition.
      */
-    void push_item(std::string_view partition_key, T item);
+    void push_item(std::string_view partition_key, T item) {
+        partitions_[std::string{partition_key}].push_back(item);
+    }
 
     /**
      * @brief Get an item from a partition, with an offset from the last item of
      * the partition.
      */
-    std::expected<T, MQErrors> get_item(std::string_view partition_key, size_t offset) const;
+    std::expected<T, MQErrors> get_item(std::string_view partition_key, size_t offset) const {
+        auto partition_it = partitions_.find(std::string {partition_key});
+        if (partition_it == partitions_.end()) {
+            return std::unexpected(MQErrors{
+                MQErrorTypes::PartitionKeyNotExist,
+                std::format("Partition of key \"{}\" does not exist.", partition_key)
+            });
+        }
+
+        const Partition &partition = (*partition_it).second;
+        if (offset >= partition.size()) {
+            return std::unexpected(MQErrors {
+                MQErrorTypes::OffsetExceedPartitionSize,
+                std::format("Partition of key \"{}\" have size = {}, where offset = ", partition_key, partition.size(), offset)
+            });
+        }
+
+        return partition[partition.size() - offset];
+    }
 
     std::expected<T, MQErrors> get_last_item(std::string_view partition_key) const { return get_item(partition_key, 0); }
 
