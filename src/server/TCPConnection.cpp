b86c7b0e1b18806f4c467097d05c09202b3a2dce
Luong Thang Nguyen <z5402969@ad.unsw.edu.au>
2026-08-09 21:43:00 +1000

    chore: wraps cli parser with try/catch, to handle uncaught exception gracefully

--- src/server/TCPConnection.cpp
+++ src/server/TCPConnection.cpp
@@ -21,55 +21,61 @@ void TCPConnection::start() {
     SPDLOG_INFO("Welcome to MessageQueue!");
 
     TopicManager topic_manager{};
-    while (true) {
-        asio::streambuf buffer;
-
-        asio::read_until(socket_, buffer, '\n');
-        std::istream line_stream{&buffer};
-
-        std::string reply;
-        std::string command;
-        line_stream >> command;
-
-        if (command == "topic") {
-            std::string operation;
-            line_stream >> operation;
-
-            if (operation == "create") {
-                std::string name;
-                line_stream >> name;
-
-                if (const auto err = topic_manager.create_topic(name)) {
-                    reply = std::format("Topic \"{}\" created successfully.", name);
-                } else {
-                    reply = std::format("Error: {}", err.value().to_string());
+    try {
+        while (true) {
+            asio::streambuf buffer;
+    
+            asio::read_until(socket_, buffer, '\n');
+            std::istream line_stream{&buffer};
+    
+            std::string reply;
+            std::string command;            
+            line_stream >> command;
+    
+            if (command == "topic") {
+                std::string operation;
+                line_stream >> operation;
+    
+                if (operation == "create") {
+                    std::string name;
+                    line_stream >> name;
+    
+                    if (const auto err = topic_manager.create_topic(name)) {
+                        reply = std::format("Error: {}", err.value().to_string());
+                    } else {
+                        reply = std::format("Topic \"{}\" created successfully.", name);
+                    }
+                } else if (operation == "populate") {
+                    std::string name;
+                    std::string partition_key;
+                    DefaultPartitionItem item;
+    
+                    line_stream >> name >> partition_key >> item;
+                    if (const auto err = topic_manager.populate(name, partition_key, item)) {
+                        reply = std::format("Error: {}", err.value().to_string());
+                    } else {
+                        reply = std::format("Item added to topic \"{}\" at partition \"{}\"", name, partition_key);
+                    }
+                } else if (operation == "consume") {
+                    std::string name;
+                    std::string partition_key;
+                    size_t offset;
+    
+                    line_stream >> name >> partition_key >> offset;
+                    if (const auto exp_item = topic_manager.consume(name, partition_key, offset)) {
+                        reply = std::format("{}", exp_item.value());
+                    } else {
+                        reply = std::format("Error: {}", exp_item.error().to_string());
+                    }
                 }
-            } else if (operation == "populate") {
-                std::string name;
-                std::string partition_key;
-                DefaultPartitionItem item;
-
-                line_stream >> name >> partition_key >> item;
-                if (const auto err = topic_manager.populate(name, partition_key, item)) {
-                    reply = std::format("Item added to topic \"{}\" at partition \"{}\"", name, partition_key);
-                } else {
-                    reply = std::format("Error: {}", err.value().to_string());
-                }
-            } else if (operation == "consume") {
-                std::string name;
-                std::string partition_key;
-                size_t offset;
-
-                line_stream >> name >> partition_key >> offset;
-                if (const auto exp_item = topic_manager.consume(name, partition_key, offset)) {
-                    reply = std::format("{}", exp_item.value());
-                } else {
-                    reply = std::format("Error: {}", exp_item.error().to_string());
-                }
+            } else if (command == "quit") {
+                break;
             }
-        } else if (command == "quit") {
-            break;
+
+            asio::write(socket_, asio::buffer(reply));
         }
+    } catch (std::exception& e) {
+        SPDLOG_ERROR("{}", e.what());
     }
 
     SPDLOG_INFO("Connection closed!");
